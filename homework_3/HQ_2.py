10cb078d0ee10d496c17b4f6dcba3275b2ba2280
IkuzO <Kuznetsovilya469@yandex.ru>
2022-10-04 22:00:17 +0300

    Задание 2

---
+++ homework_3/HQ_2.py
@@ -0,0 +1,30 @@
+'''Напишите программу, 
+которая найдёт произведение пар чисел списка. 
+Парой считаем первый и последний элемент,
+второй и предпоследний и т.д.
+Пример:
+- [2, 3, 4, 5, 6] => [12, 15, 16];
+- [2, 3, 5, 6] => [12, 15]'''
+import random
+
+
+def ad(ni):
+    for i in range(0, ni):
+        n.append(random.randint(-20, 20))
+
+
+def av(na):
+    for i in range(0, na):
+        if (i < (na/2)):
+            l = n[i]*n[(na-1)-i]
+            s.append(l)
+    print(s)
+
+
+n = list()
+s = list()
+print('Введите кол-во элементов')
+num = int(input())
+ad(num)
+print(n)
+av(num)
