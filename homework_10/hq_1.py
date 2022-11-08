4952980730465f4b1ccc970e83a5d52f1152afb5
IkuzO <Kuznetsovilya469@yandex.ru>
2022-11-08 23:36:10 +0300

    калькулятор

---
+++ homework_10/hq_1.py
@@ -0,0 +1,7 @@
+# Калькулятор рациональных и комплексных чисел, добавить систему логирования
+
+
+import telebot.types
+from telebot import TeleBot
+
+bot = TeleBot('5498581242:AAF1bnFZdJAJTYqZzf96Uc_PsTqF834PtrQ')
